03221833f184e1200d4ca3c45f0d1c8f5042dca3
xue <918910251@qq.com>
2017-02-26 20:59:37 +0800

    这是个小demo

---
+++ a.js
@@ -0,0 +1,8 @@
+var box=document.getElementById('box');
+
+box.onmouseover=function(){
+    box.style.backgroundColor="pink";
+}
+box.onmouseleave=function(){
+    box.style.backgroundColor="blue";
+}
